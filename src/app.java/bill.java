d18bb33165f95d25e283deceee1dc14cb2729eb0
Bassamaljuhani <120460327+Bassamaljuhani@users.noreply.github.com>
2023-05-19 20:21:30 +0000

    Printbill

--- src/app.java/bill.java
+++ src/app.java/bill.java
@@ -3,9 +3,25 @@ public class bill extends order {
     public bill() {
         super();
         
-        //TODO Auto-generated constructor stub
+     
     }
+    public void Printbill() {
 
+
+    system.out.println("Name of coustomer");
+    getName();
+    system.out.println("Number phone of coustomer");
+    getNumOfcusm();
+    system.out.println("Order number");
+    getNumOforder();
+    system.out.println("Order type");
+    getTypeofOrder();
+    system.out.println("Number of person");
+    getpersons();
+    getpirce();
+
+
+    }
     
     
 
